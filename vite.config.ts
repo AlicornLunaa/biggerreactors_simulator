cac52c3a020ef6fb157ca11ad99c69b454367983
Luna <alicornlunaa@gmail.com>
2023-02-22 23:20:04 -0600

    Fixed config problem

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://alicornlunaa.github.io/reactor_simulator",
+  base: "https://alicornlunaa.github.io/biggerreactors_simulator",
 })
