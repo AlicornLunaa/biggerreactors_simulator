05a0bf8af22e0d437556c76b93d99a9f28571a25
AlicornLunaa <alicornlunaa@gmail.com>
2024-03-15 13:31:57 -0500

    Ironed out some basic bugs and added size setting

--- src/main.ts
+++ src/main.ts
@@ -20,15 +20,23 @@ let clicking = false;
 // Config modal window
 let newBtn = document.querySelector<HTMLButtonElement>("#new-reactor-btn")!;
 let modalWindow = document.querySelector<HTMLButtonElement>("#reactor-modal")!;
+let widthInput = document.querySelector<HTMLInputElement>("#reactor-width-txt")!;
+let depthInput = document.querySelector<HTMLInputElement>("#reactor-depth-txt")!;
+let heightInput = document.querySelector<HTMLInputElement>("#reactor-height-txt")!;
 newBtn.addEventListener("click", () => {
   modalWindow.style.display = "block";
+  widthInput.value = `${reactorWidth}`;
+  depthInput.value = `${reactorDepth}`;
+  heightInput.value = `${reactorHeight}`;
 });
-
 window.onclick = function(event) {
   if (event.target == modalWindow) {
     modalWindow.style.display = "none";
   }
-}
+};
+widthInput.addEventListener("change", () => reactorWidth = Number.parseInt(widthInput.value));
+depthInput.addEventListener("change", () => reactorDepth = Number.parseInt(depthInput.value));
+heightInput.addEventListener("change", () => reactorHeight = Number.parseInt(heightInput.value));
 
 // Populate element list
 let elementsDiv = document.querySelector<HTMLDivElement>(".material-list")!;
@@ -51,13 +59,14 @@ reactorCanvas.width = reactorCanvas.offsetWidth;
 reactorCanvas.height = reactorCanvas.offsetHeight;
 let ctx = reactorCanvas.getContext("2d")!;
 
+const get_mouse_pos = (ev: MouseEvent) => { x:return { x: ev.clientX - reactorCanvas.getBoundingClientRect().left, y: ev.clientY - reactorCanvas.getBoundingClientRect().top }; };
 const draw_canvas = () => {
   ctx.clearRect(0, 0, reactorCanvas.width, reactorCanvas.height);
 
   for(let i = 0; i < reactorWidth + 2; i++) for(let k = 0; k < reactorWidth + 2; k++){
     let cellWidth = reactorCanvas.width / (reactorWidth + 2);
     let cellHeight = reactorCanvas.height / (reactorHeight + 2);
-    let cellGap = 5;
+    let cellGap = 3;
     
     let cellX = cellWidth * i + cellGap;
     let cellY = cellHeight * k + cellGap;
@@ -78,7 +87,7 @@ const draw_canvas = () => {
     
     ctx.fillRect(cellX, cellY, cellW, cellH);
     
-    ctx.font = "20px Arial";
+    ctx.font = `${30 * (cellWidth / 195)}px Arial`;
     ctx.textAlign = "center"
     ctx.fillStyle = "#ffffff";
     ctx.fillText("Hello world", cellWidth * i + cellWidth / 2, cellHeight * k + cellHeight / 2 + 10);
@@ -86,9 +95,14 @@ const draw_canvas = () => {
 }
 draw_canvas();
 
+window.addEventListener("resize", () => {
+  reactorCanvas.width = reactorCanvas.offsetWidth;
+  reactorCanvas.height = reactorCanvas.offsetHeight;
+  draw_canvas();
+});
+
 reactorCanvas.addEventListener("mousemove", (ev) => {
-  const get_mouse_pos = () => { x:return { x: ev.clientX - reactorCanvas.getBoundingClientRect().left, y: ev.clientY - reactorCanvas.getBoundingClientRect().top }; };
-  let pos = get_mouse_pos();
+  let pos = get_mouse_pos(ev);
   mouseX = pos.x;
   mouseY = pos.y;
   selectedI = Math.floor(mouseX / (reactorCanvas.width / (reactorWidth + 2)));
@@ -97,8 +111,7 @@ reactorCanvas.addEventListener("mousemove", (ev) => {
 });
 
 reactorCanvas.addEventListener("mousedown", (ev) => {
-  const get_mouse_pos = () => { x:return { x: ev.clientX - reactorCanvas.getBoundingClientRect().left, y: ev.clientY - reactorCanvas.getBoundingClientRect().top }; };
-  let pos = get_mouse_pos();
+  let pos = get_mouse_pos(ev);
   mouseX = pos.x;
   mouseY = pos.y;
   selectedI = Math.floor(mouseX / (reactorCanvas.width / (reactorWidth + 2)));
@@ -108,8 +121,7 @@ reactorCanvas.addEventListener("mousedown", (ev) => {
 });
 
 reactorCanvas.addEventListener("mouseup", (ev) => {
-  const get_mouse_pos = () => { x:return { x: ev.clientX - reactorCanvas.getBoundingClientRect().left, y: ev.clientY - reactorCanvas.getBoundingClientRect().top }; };
-  let pos = get_mouse_pos();
+  let pos = get_mouse_pos(ev);
   mouseX = pos.x;
   mouseY = pos.y;
   selectedI = Math.floor(mouseX / (reactorCanvas.width / (reactorWidth + 2)));
