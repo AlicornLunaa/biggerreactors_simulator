4f2e2daae051a9464b971500ad5928c4010564df
AlicornLunaa <alicornlunaa@gmail.com>
2024-03-15 23:07:17 -0500

    Added extremereactors 1.20

---
+++ src/simulation/ReactorInterface.ts
@@ -0,0 +1,7 @@
+/**
+ * This interface is meant to be used as the compatability layer between the
+ * ported java code and the javascript for the website.
+ */
+interface ReactorInterface {
+    
+};
